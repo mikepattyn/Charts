145ddee83d421befd107f9f86e15a18e75fc0ad0
Mike Pattyn <mike.pattyn1@gmail.com>
2021-02-22 19:16:29 +0100

    comment this again :)

--- Source/Charts/Renderers/HorizontalBarChartRenderer.swift
+++ Source/Charts/Renderers/HorizontalBarChartRenderer.swift
@@ -371,10 +371,10 @@ open class HorizontalBarChartRenderer: BarChartRenderer
                             break
                         }
                         
-                        if !viewPortHandler.isInBoundsX(rect.origin.x)
-                        {
-                            continue
-                        }
+                        // if !viewPortHandler.isInBoundsX(rect.origin.x)
+                        // {
+                        //     continue
+                        // }
                         
                         if !viewPortHandler.isInBoundsBottom(rect.origin.y)
                         {
